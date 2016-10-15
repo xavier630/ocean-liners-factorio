b137e18f9e962dd2bee9358555cc550ad093e54f
Xavier Downs <xavier630@gmail.com>
2016-10-15 15:26:16 +1300

    Stopped sea-oil spawning too close together

--- OceanLiners_0.1.2/control.lua
+++ OceanLiners_0.1.2/control.lua
@@ -19,6 +19,8 @@ face_down=function(car)
   car.orientation =  0.5
   return true
 end
+
+
 calc_orientation=function(player, car)
   Cx = car.position.x
   Cy = car.position.y
@@ -26,7 +28,6 @@ calc_orientation=function(player, car)
   Py = player.position.y
   dx = Cx - Px
   dy = Cy - Py
-
   orientation = math.atan2(dx, dy)
   orientation = math.deg(orientation) --convert to degrees
   orientation  = (orientation / 360)--convert so out of 1
@@ -47,10 +48,9 @@ calc_orientation=function(player, car)
   end
   car.orientation = orientation
   return true
-
 end
 
- script.on_event(defines.events.on_player_driving_changed_state, function(event)
+ --[[script.on_event(defines.events.on_player_driving_changed_state, function(event)
    --game.players[event.player_index].color = {r=0.1, g=0.1, b=0.9, a=0.8}
    local player = game.players[event.player_index]
    --player.surface.print("test printing")
@@ -71,12 +71,9 @@ end
        --last_boat.speed = 0.05
      end
 
-
-
-
    end
 
- end)
+ end)]]
  script.on_event(defines.events.on_tick, function(event)
   --[[ player = game.players[1]
    if player.character.vehicle and player.character.vehicle.name == "paddle-steamer" then
@@ -88,7 +85,7 @@ end
      player = game.players[1] --TODO give to the right player - will be easier when API is updated so that the event features a player
      player.character.insert{name="cannon-shell", count=8}
    end
- end) --[[
+ end)
  script.on_event(defines.events.on_player_crafted_item, function(event) --Script to test for entities around the player
      local player = game.players[event.player_index]
      local surface = player.surface
@@ -102,20 +99,37 @@ end
       surface.print(entity.name .. " " .. entity.type)
      end
 
- end)]]
+ end)
  script.on_event(defines.events.on_chunk_generated, function(event) --delete all resources and plants on water except oil
    local box = event.area
    local surface = event.surface
    local entities = surface.find_entities_filtered{area = box, type = "resource"}
    local entities2 = surface.find_entities_filtered{area = box, type = "decorative"}
-   for k,v in pairs(entities2) do table.insert(entities, v) end
+   local to_be_destroyed = {}
+   for k,v in pairs(entities2) do table.insert(entities, v) end --merge the two tables.
    for _, i in pairs(entities) do
-     local pos = i.position
-     local tile = surface.get_tile(pos.x, pos.y)
-     if tile.name == "deepwater" then
-       if (i.name ~= "crude-oil" and i.name ~= "sea-oil") then
-        i.destroy()
-       end
-     end
-   end
+     -- if (i.valid) -- then only needed when deleting surrounding oil, not the current oil
+       local pos = i.position
+       local tile = surface.get_tile(pos.x, pos.y)
+       if (i.name == "sea-oil") then --Make sure oil isn't too close together
+          local area = {{pos.x - 3, pos.y - 3}, {pos.x + 3, pos.y + 3}}
+          local neighbours = surface.find_entities_filtered({area=area, name = "sea-oil"})
+          --surface.print(neighbours[1])
+
+          for key, j in pairs(neighbours) do
+            surface.print("Neighbour++")
+            if (j ~= i) then
+              --Can either delete the surround oil or delete this oil.
+              i.destroy()
+              break
+              --j.destroy()
+            end
+          end
+       else
+         if (tile.name == "deepwater") then -- Destroy if not crude oil and over the water
+          i.destroy()
+         end
+       end --end if
+    --end
+   end --end for
  end)

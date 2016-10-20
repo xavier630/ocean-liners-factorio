90560e57f22e70dc3e905b72f8f7107399c58388
Xavier Downs <xavier630@gmail.com>
2016-10-21 09:31:19 +1300

    Added gitignore

--- OceanLiners_0.1.8/config.lua
+++ OceanLiners_0.1.8/config.lua
@@ -2,4 +2,6 @@
 --Note as of version 0.1.5, biters can move at full speed when swimming while players are heavily slowed. Have fun!
 --If you want biters to cross water set to true
 biters_cross_water = false
+
+--If you want players to be able to swim slowly through water to get to boats, set this to true.
 player_swimming = true

c50db496bb8fd750c2d151afdbabc0f0c06ad571
Xavier Downs <xavier630@gmail.com>
2017-07-22 17:18:51 +1200

    Remove accidental leftover config

--- src/config-code.lua
+++
@@ -1,9 +0,0 @@
-require("config")
-if not (biters_cross_water) then
-  require("functions.biters-cross-water")
-end
-if (player_swimming) then
-  data.raw["player"]["player"].collision_mask={"player-layer"}
-else
-  data.raw["player"]["player"].collision_mask={"player-layer", "water-tile"}
-end
